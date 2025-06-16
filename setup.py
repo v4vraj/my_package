42ae7b630b66f57072f8684fe36818c3922f565b
vraj1763@gmail.com <vraj1763@gmail.com>
2025-06-17 02:34:14 +0530

    final

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="my_package",
     version="0.1",
-    packages=find_packages(),
-    description="A package hosted on GitHub",
-    long_description=open("README.md").read(),
+    packages=find_packages(),  # Automatically finds `my_package/`
+    description="Your package description",
     author="Your Name",
-    url="https://github.com/your-v4vraj/my_package",
-    install_requires=[],  # Add dependencies here (e.g., "numpy")
+    author_email="your@email.com",
+    url="https://github.com/v4vraj/my_package",
 )
